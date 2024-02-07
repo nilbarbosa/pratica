14d1f9499c3d2c504f933b5008c4e3f123c5d5f3
nilbarbosa <149343130+nilbarbosa@users.noreply.github.com>
2024-02-07 09:21:41 -0300

    criei os botões e suas funções, iniciei o estilo.

---
+++ freeCode_play_game_js/script.js
@@ -0,0 +1,25 @@
+const button1=document.querySelector("#button1")
+const button2=document.querySelector("#button2")
+const button3=document.querySelector("#button3")
+const text=document.querySelector("#text")
+const xpText=document.querySelector("#xpText")
+const healthText=document.querySelector("#healthText")
+const goldText=document.querySelector("#goldText")
+const monsterStats=document.querySelector("#monsterStats")
+const monsterName=document.querySelector("#monsterName")
+const monsterHealthText=document.querySelector("#monsterHealth")
+
+
+function goStore() {
+    console.log("Going to store.");
+}
+
+function goCave() {
+    console.log("Going to cave.");
+}
+
+//initialize buttons
+
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
